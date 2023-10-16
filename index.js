58cb48df44a4e407f97fc4549d0b65ca6a8c1f28
MahalakshmiS97 <mahalakshmivarma97@gmail.com>
2023-10-16 12:04:55 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require('express') //commonJS Import older version
-// import express from 'express'//ES Module Import add "type": "module" in package.json
+const express = require('express') 
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT
+
 const AppRoutes = require('./src/routes')
 const app = express()
 app.use(express.json())
-
 app.use('/',AppRoutes)
 
-
-app.listen(8000,()=>console.log("Server listening to port 8000"))
-
-// npm init
-// npm i express
-// "start": "node index.js",
+app.listen(PORT,()=>console.log(`Server listening to port ${PORT}`))
